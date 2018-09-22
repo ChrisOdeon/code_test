613daa67bf702585484741c99d8ce472cb74a55d
ChrisOdeon <odeonchris@gmail.com>
2018-09-22 17:03:09 +0100

    Added initial api class

---
+++ app/services/mic/api.rb
@@ -0,0 +1,37 @@
+require 'httparty'
+
+class Mic::Api
+  APIURL = ENV['LEAD_API_URI']
+  TOKEN = ENV['LEAD_API_ACCESS_TOKEN']
+  USERID = ENV['LEAD_API_PGUID']
+  ACCNAME = ENV['LEAD_API_PACCNAME']
+  PARTNERNAME = ENV['LEAD_API_PPARTNER']
+
+  def self.request_callback(name:, business_name:, tel_number:, email:)
+    callback_url = APIURL + '/api/v1/create'
+    HTTParty.post(callback_url,
+      body: request_body(name, business_name, tel_number, email),
+      headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    )
+  end
+
+  private
+  
+  def self.request_body(name, business_name, tel_number, email)
+    URI.encode_www_form(
+      {
+        access_token: TOKEN,
+        pGUID: USERID,
+        pAccName: ACCNAME,
+        pPartner: PARTNERNAME,
+        name: name,
+        business_name: business_name,
+        telephone_number: tel_number,
+        email: email,
+        contact_time: Time.now.utc.strftime("%Y-%m-%d %T"),
+        notes: '',
+        reference: ''
+      }
+    )
+  end
+end
---
+++ spec/services/mic/api_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Mic::Api do
+   context 'I make an API callback request' do
+    let(:result) do
+      VCR.use_cassette 'mic_api/api_response' do
+        Mic::Api.request_callback(
+          name: 'Test Name',
+          business_name: 'Test Business Name',
+          tel_number: '01234567890',
+          email: 'testname@example.com'
+        )
+      end
+    end
+
+    it 'should work' do
+      expect(result.parsed_response["message"]).to eq "Enqueue success"
+    end
+  end
+end
